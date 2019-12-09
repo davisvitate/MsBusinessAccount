25e9aff517722ef9904faaff44cee2bc14bd7cd3
davisvitate <davisvitate@gmail.com>
2019-12-09 15:30:56 -0500

    first commit

---
+++ src/main/java/com/microservices/BusinessAccount/BusinessAccountApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.BusinessAccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessAccountApplication.class, args);
+	}
+
+}
